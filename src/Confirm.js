578c692b94b68be41dbccb79c781f9fadfc52860
Greg Schechter <diabolomaster@gmail.com>
2016-03-10 11:37:36 -0800

    Use react-bootstrap components

--- src/Confirm.js
+++ src/Confirm.js
@@ -1,30 +1,14 @@
 var React = require('react');
-import { Button } from 'react-bootstrap';
-var Modal = require('Modal').Modal;
-var ModalHeader = require('Modal').ModalHeader;
-var ModalContent = require('Modal').ModalContent;
-var ModalFooter = require('Modal').ModalFooter;
-
+var { Button, Modal } = require('react-bootstrap');
 
 var Confirm = React.createClass({
-    contextTypes: {
-        theme: React.PropTypes.object.isRequired,
-    },
-
     propTypes: {
-        bsStyle: React.PropTypes.string,
+        body: React.PropTypes.any.isRequired,
         buttonText: React.PropTypes.string,
         confirmText: React.PropTypes.string,
-        message: React.PropTypes.any.isRequired,
         onConfirm: React.PropTypes.func.isRequired,
         title: React.PropTypes.string.isRequired,
         visible: React.PropTypes.bool,
-    },
-
-    getDefaultProps() {
-        return {
-            bsStyle: 'default'
-        };
     },
 
     getInitialState() {
@@ -65,24 +49,26 @@ var Confirm = React.createClass({
                 onClick: this.onButtonClick,
             });
         } else {
-            content = <Button bsStyle={this.props.bsStyle} onClick={this.onButtonClick}>{this.props.buttonText}</Button>;
+            content = (
+                <Button onClick={this.onButtonClick}>
+                    {this.props.buttonText}
+                </Button>
+            );
         }
 
 
         return (
             <span style={this.props.style}>
                 {content}
-                <Modal isOpened={this.state.isOpened} onClose={this.onClose}>
-                    <ModalHeader title={this.props.title} />
-                    <ModalContent>
-                        {this.props.message}
-                    </ModalContent>
-                    <ModalFooter>
+                <Modal show={this.state.isOpened} onHide={this.onClose}>
+                    <Modal.Header title={this.props.title} />
+                    <Modal.Body>
+                        {this.props.body}
+                    </Modal.Body>
+                    <Modal.Footer>
                         <Button bsStyle="default" onClick={this.onClose}>Cancel</Button>
-                        {this.props.confirmText ? (
-                            <Button bsStyle="danger" onClick={this.onConfim}>{this.props.confirmText}</Button>
-                        ) : null}
-                    </ModalFooter>
+                        <Button bsStyle="danger" onClick={this.onConfim}>{this.props.confirmText ? this.props.confirmText : 'Confirm'}</Button>
+                    </Modal.Footer>
                 </Modal>
             </span>
         );
